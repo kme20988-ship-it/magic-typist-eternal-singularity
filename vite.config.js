ca469ef5f62eb17343aea933a98734805d71fa45
岩田航明 <iwatakoumei@iwatawatarumeinoMacBook-Air.local>
2026-01-14 17:52:48 +0900

    🚀 DEPLOY: Configure for Vercel support and dynamic base path

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,15 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/magic-typist-eternal-singularity/',
-  plugins: [
-    react(),
-    wasm(),
-    topLevelAwait()
-  ],
+export default defineConfig(({ mode }) => {
+  const isGitHubPages = process.env.DEPLOY_TARGET === 'gh-pages';
+
+  return {
+    base: isGitHubPages ? '/magic-typist-eternal-singularity/' : '/',
+    plugins: [
+      react(),
+      wasm(),
+      topLevelAwait()
+    ],
+  }
 })
